e1fe876e65ff559fe9852a6fc32533d35b29c9d6
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-05-04 13:32:17 -0600

    update: print old and new mac

--- mac_changer.py
+++ mac_changer.py
@@ -3,6 +3,8 @@
 import subprocess
 # optparse module allows us to parse command-line options (use arguments)
 import optparse
+# re module allows us to use regular expressions
+import re
 
 
 def get_arguments():
@@ -24,8 +26,28 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+
 opts = get_arguments()
+
+current_mac = get_current_mac(opts.interface)
+print("Current MAC = " + str(current_mac))
+
 change_mac(opts.interface, opts.new_mac)
+
+current_mac = get_current_mac(opts.interface)
+if current_mac == opts.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
+
 
 # interface = options.interface  # interface = raw_input("interface > ")  # input() for python 3
 # new_mac = options.new_mac  # new_mac = raw_input("new MAC > ")
